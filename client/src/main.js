f00052bd3e74a74204e04ab0ecc49fa2bdb76f64
Wey Gu <littlewey@gmail.com>
2019-02-21 21:08:06 +0800

    vue vuetify initialized

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'vue-material/dist/vue-material.min.css';
-import Vue from 'vue';
-import VueMaterial from 'vue-material';
-import App from './App';
-import router from './router';
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.use(VueMaterial);
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
-});
+  render: h => h(App),
+}).$mount('#app')
