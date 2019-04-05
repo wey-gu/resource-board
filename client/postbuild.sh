960b1318b0e1c7a0464ddaebc1a5e46ec1e5f30c
Wey Gu <littlewey@gmail.com>
2019-04-05 23:40:02 +0800

    fix npm run build on macOS

--- client/postbuild.sh
+++ client/postbuild.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-# sed 's/fonts.googleapis.com/fonts.loli.net/g' dist/index.html > dist/index.html
+# sed in macOS is different from the one in Linux, cannot use sed -i
+sed 's/fonts.googleapis.com/fonts.loli.net/g' dist/index.html > dist/index.html_replaced
+mv dist/index.html_replaced dist/index.html
 rm dist/favicon.ico
