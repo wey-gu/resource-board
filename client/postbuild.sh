08126c2f56f8e1a1a4864310069b2cb1e5652cc2
Wey Gu <littlewey@gmail.com>
2019-04-06 15:52:54 +0800

    added icon

--- client/postbuild.sh
+++ client/postbuild.sh
@@ -2,4 +2,4 @@
 # sed in macOS is different from the one in Linux, cannot use sed -i
 # sed 's/fonts.googleapis.com/fonts.loli.net/g' dist/index.html > dist/index.html_replaced
 # mv dist/index.html_replaced dist/index.html
-rm dist/favicon.ico
+cp src/assets/logo.png dist/favicon.ico
